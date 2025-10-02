1071296127b0bccb2beeb454d516ad0c57a9040e
Sikelela Nkohla <sikelela.nkohla@entelect.co.za>
2025-10-02 18:01:27 +0200

    initial commit

---
+++ src/test/java/za/co/lemonade/habitStack/HabitStackApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.lemonade.habitStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
